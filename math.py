c3dee4ad69a78426c627a4cf6381d013b1eaa9db
akshay <akshaykonapure12@gmail.com>
2021-01-29 17:15:22 +0530

    Div impl

--- math.py
+++ math.py
@@ -1,11 +1,11 @@
 def add(x,y):
     return x+y
 def subtract(x,y):
-    if x<y
+    if x<y:
 	return error 
     return x-y
 def multiply(x,y):
     return x*y
     
 def divide(x,y):
-    pass
+    return x/y
